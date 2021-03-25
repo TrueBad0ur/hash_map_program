1cbd6a519ce5902db5541e7e04ed22def94a8382
truebad0ur <pieceofcakecupofcoffee@gmail.com>
2021-03-25 12:05:02 +0300

    Add main code

--- hash_map_program/src/main.rs
+++ hash_map_program/src/main.rs
@@ -1,3 +1,38 @@
+//#![allow(warnings)]
+use std::collections::HashMap;
+use std::io;
+use std::collections::hash_map::RandomState;
+
+fn say_hello() {
+    println!("Hello!\nEnter your list of words: ");
+}
+
+fn get_input() -> String {
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect(
+        "Failed to get words!",
+    );
+
+    guess
+}
+
+fn create_hash_map(user_input: &String) -> HashMap<&str, i32, RandomState> {
+    let mut map: HashMap<&str, i32, RandomState> = HashMap::new();
+
+    for word in user_input.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    map
+}
+
 fn main() {
-    println!("Hello, world!");
+    say_hello();
+    let mut user_input = String::from(get_input());
+    user_input = user_input.replace(",", "");
+    let hash_map = create_hash_map(&user_input);
+
+    println!("{:?}", hash_map);
+
 }
