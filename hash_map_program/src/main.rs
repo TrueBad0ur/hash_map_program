dcb5449cea44687f0c0fc19f0d1d850008519813
truebad0ur <pieceofcakecupofcoffee@gmail.com>
2021-03-25 14:39:04 +0300

    Change to Most frequent word via HashMap

--- hash_map_program/src/main.rs
+++ hash_map_program/src/main.rs
@@ -27,12 +27,27 @@ fn create_hash_map(user_input: &String) -> HashMap<&str, i32, RandomState> {
     map
 }
 
+fn get_most_frequent(map: HashMap<&str, i32, RandomState>) -> String {
+    let mut max_val = i32::MIN;
+    let mut max_val_key = String::from("Nothing!");
+
+    for (k, v) in map {
+        if v > max_val {
+            max_val = v;
+            max_val_key = String::from(k);
+        }
+    }
+    max_val_key
+}
+
 fn main() {
     say_hello();
     let mut user_input = String::from(get_input());
     user_input = user_input.replace(",", "");
     let hash_map = create_hash_map(&user_input);
 
-    println!("{:?}", hash_map);
+    let most_frequent = get_most_frequent(hash_map);
+
+    println!("Most frequent word is \"{}\"", most_frequent);
 
 }
